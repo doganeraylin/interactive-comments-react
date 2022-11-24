31a5c16980cd3a0550dbb38e5e63162048bbb671
doganeraylin <doganeraylinn@gmail.com>
2022-11-24 12:12:56 +0200

    rendered add comment component

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,40 @@ import './App.css'
 import data from "./data"
 import Comment from './components/Comment'
 import Reply from './components/Reply'
-// import AddComment from './components/AddComment'
+import AddComment from './components/AddComment'
 
 function App() {
-
   return(
-    data.comments.map((comment) => {
-        return (
-            <>
-                <Comment
-                    key={comment.id}
-                    content={comment.content}
-                    createdAt={comment.createdAt}
-                    username={comment.user.username}
-                    img={comment.user.image.png}
-                    counter={comment.score}
-                />
+    <div>
+        {data.comments.map((comment) => {
+            return (
+                <>
+                    <Comment
+                        key={comment.id}
+                        content={comment.content}
+                        createdAt={comment.createdAt}
+                        username={comment.user.username}
+                        img={comment.user.image.png}
+                        counter={comment.score}
+                    />
 
-                {comment.replies.map((reply) => 
-                    <Reply
-                        key={reply.id}
-                        content={reply.content}
-                        createdAt={reply.createdAt}
-                        username={reply.user.username}
-                        img={reply.user.image.png}
-                        counter={reply.score}
-                        replyingTo={reply.replyingTo}
-                    />  
-                )}
-                {/* <AddComment /> */}
-            </>
-        )
-    })
+                    {comment.replies.map((reply) => 
+                        <Reply
+                            key={reply.id}
+                            content={reply.content}
+                            createdAt={reply.createdAt}
+                            username={reply.user.username}
+                            img={reply.user.image.png}
+                            counter={reply.score}
+                            replyingTo={reply.replyingTo}
+                        />  
+                    )}                    
+                </>   
+            )
+        })}
+        <AddComment/>
+    </div>
   )
-
 }
 
 export default App
