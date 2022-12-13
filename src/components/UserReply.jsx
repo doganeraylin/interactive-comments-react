e0d551f0dae4a64138bdfc4125bb0ac6d6b1cb79
doganeraylin <doganeraylinn@gmail.com>
2022-12-13 13:31:25 +0200

    created userReply component

---
+++ src/components/UserReply.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+import DeleteModal from "./DeleteModal"
+
+const UserReply = (
+    {content,
+    createdAt,
+    img,
+    username,
+    counter,
+    replyingTo,
+    handleToggle
+   }
+) => {
+
+    const [newScore, setNewScore] = useState(counter)
+
+    function incrementScore() {
+        return setNewScore((prevScore) => prevScore + 1)
+    }
+
+    function decrementScore() {
+        return setNewScore((prevScore) => prevScore - 1)
+    }
+
+
+
+    return (
+    <>
+        <div className="card-container">
+            <div className="vote-container vote-container-userreply">
+                <p className="vote" onClick={incrementScore}>+</p>
+                <p className="score">{newScore}</p>
+                <p className="vote" onClick={decrementScore}>-</p>
+            </div>
+            <div className="card-content">
+                <div className="card-top-container ">
+                    <div className="card-top-info">  
+                        <img src={img} className="icon"></img>
+                        <p className="username">{username}</p>
+                        <p className="you-tag">you</p>
+                        <p className="createdAt">2 days ago</p>
+                    </div>
+                    <div className="reply-btn-container">
+                        <img src="../../public/images/icon-delete.svg" className="delete-icon"></img>
+                        <p className="delete-text" onClick={handleToggle}>Delete</p>
+                        <img src="../../public/images/icon-edit.svg" className="edit-icon"></img>
+                        <p className="reply-text">Edit</p>
+                    </div>
+                </div>
+                <div className="card-text">
+                <p className="content">{content}</p>
+                </div>
+            </div>
+        </div>
+        
+        </>
+    )
+}
+
+export default UserReply
