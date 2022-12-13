50cfe24a4f8124a7a6728b7b1da427e9b5f2fcf1
doganeraylin <doganeraylinn@gmail.com>
2022-12-13 13:30:18 +0200

    addreply rendered

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react"
+import AddReply from "./AddReply"
 
 const Reply = (
     {content,
     createdAt,
     img,
     username,
-    replyingTo,
-    counter}
+    incrementScore,
+    counter,
+    replyingTo
+    }
 ) => {
+
+    const [isReply, setIsReply] = useState(false)
 
     const [newScore, setNewScore] = useState(counter)
 
@@ -19,32 +24,37 @@ const Reply = (
         return setNewScore((prevScore) => prevScore - 1)
     }
 
-    return (<>
-        <div className="card-container reply-card-container">
-            <div className="vote-container">
-                <p className="vote" onClick={incrementScore}>+</p>
-                <p className="score">{newScore}</p>
-                <p className="vote" onClick={decrementScore}>-</p>
-            </div>
-            <div className="card-content">
-                <div className="card-top-container ">
-                    <div className="card-top-info">  
-                        <img src={img} className="icon"></img>
-                        <p className="username">{username}</p>
-                        <p className="createdAt">{createdAt}</p>
-                    </div>
-                    <div className="reply-btn-container">
-                        <img src="../../public/images/icon-reply.svg" className="reply-icon"></img>
-                        <p className="reply-text">Reply</p>
-                    </div>
-                </div>
-                <div className="card-text">
-                    <p className="content"><span className="replyingTo">{`@${replyingTo}`}</span>{` ${content}`}</p>
-                </div>
-            </div>
-          
+    function clickReply () {
+       setIsReply(true)
+    }
+
+    return (
+    <>
+    <div className="card-container">
+        <div className="vote-container">
+            <p className="vote" onClick={incrementScore}>+</p>
+            <p className="score">{newScore}</p>
+            <p className="vote" onClick={decrementScore}>-</p>
         </div>
-        </>
+        <div className="card-content" >
+            <div className="card-top-container">
+                <div className="card-top-info">  
+                    <img src={img} className="icon"></img>
+                    <p className="username">{username}</p>
+                    <p className="createdAt">{createdAt}</p>
+                </div>
+                <div className="reply-btn-container">
+                    <img src="../../public/images/icon-reply.svg" className="reply-icon"></img>
+                    <p className="reply-text" onClick={clickReply}>Reply</p>
+                </div>
+            </div>
+            <div className="card-text">
+                <p className="content"><span className="replying-to">@{replyingTo} </span>{content}</p>
+            </div>
+        </div>
+    </div>
+    {isReply && <AddReply/>}
+    </>
     )
 }
 
