09d2e4ed7e30f29a9505228a40443602042ab46d
doganeraylin <doganeraylinn@gmail.com>
2022-12-13 13:29:16 +0200

    added delete comment and add comment functions

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,17 +1,89 @@
+import { useState, useContext, createContext } from "react"
+import data from "../data.json"
+import DeleteModal from "./DeleteModal"
+import UserReply from "./UserReply"
+
 const AddComment = ()  => {
 
+    const [value, setTextareaValue] = useState("")
+    const [formSubmitted, setFormSubmitted] = useState(false)
+    const [comments, setComments] = useState(data.comments.slice())
+    const [submittedComments, setSubmittedComments] = useState([])
+    const [isModalOpen, setIsModalOpen] = useState(false)
+
+    function handleSubmit(event) {
+      event.preventDefault()
+  
+      const newComment = {
+        content: value,
+        img: "../../public/images/avatars/image-juliusomo.png",
+        username: "juliusomo",
+        counter: 0
+      }
+  
+      setComments([...comments, newComment])
+      setSubmittedComments([])
+      setSubmittedComments([...submittedComments, newComment])
+      setFormSubmitted(true)
+      setTextareaValue("")
+      setIsModalOpen(false) 
+    }
+
+    function deleteComments (id) {
+        const updatedComments = submittedComments.filter((comment) => comment.id !== id)
+        setSubmittedComments(updatedComments)
+    }
+
+    function handleToggle () {
+      setIsModalOpen(!isModalOpen)
+  }
+
     return (
-        <div className="card-container">
-            <img src="../../public/images/avatars/image-juliusomo.png" className="comment-user-icon"></img>
-            <textarea 
-                className="add-comment-textarea" 
-                cols="50" 
-                rows="4" 
-                placeholder="Add a comment..."
-            ></textarea>
-            <p className="send-btn">SEND</p>
+      <>
+        <div>
+          {submittedComments.map((comment, index) => (
+            <UserReply
+              key={index}
+              content={comment.content}
+              createdAt={comment.createdAt}
+              img={comment.img}
+              username={comment.username}
+              counter={comment.counter}
+              handleToggle={handleToggle}
+             />
+          ))}
         </div>
+
+        {isModalOpen && submittedComments.map((comment, index) => (
+            <DeleteModal 
+              key={index}
+              cancelDelete = {handleToggle}
+              deleteComment={() => deleteComments(comment.id)}
+            />
+        ))}
+            
+        <div className="card-container">
+          <img src="../../public/images/avatars/image-juliusomo.png" className="comment-user-icon"></img>
+          <form id="add-comment-form" onSubmit={handleSubmit}>
+            <textarea
+              id="textarea"
+              className="add-comment-textarea"
+              cols="50"
+              rows="4"
+              placeholder="Add a comment..."
+              onChange={(event) => setTextareaValue(event.target.value)}
+              value={value}
+            >
+            </textarea>
+            <button type="submit" className="send-btn">SEND</button>
+          </form>
+        </div>
+      </>
     )
-}
- 
-export default AddComment
+  }
+  
+  export default AddComment
+  
+
+
+
