d8d9ea5bdc9a896dbca8bb1132940c699e4150f2
doganeraylin <doganeraylinn@gmail.com>
2022-12-17 12:40:09 +0200

    removed edit

--- src/components/UserReply.jsx
+++ src/components/UserReply.jsx
@@ -4,13 +4,10 @@ import ReactTimeAgo from 'react-time-ago'
 
 const UserReply = (
     {content,
-    createdAt,
     img,
     username,
     counter,
-    replyingTo,
     handleToggle,
-    date
    }
 ) => {
 
@@ -24,29 +21,27 @@ const UserReply = (
         return setNewScore((prevScore) => prevScore - 1)
     }
 
-
-
     return (
     <>
         <div className="card-container">
-            <div className="vote-container">
+            <div className="user-vote-container vote-container">
                 <p className="vote" onClick={incrementScore}>+</p>
                 <p className="score">{newScore}</p>
                 <p className="vote" onClick={decrementScore}>-</p>
             </div>
             <div className="card-content">
-                <div className="card-top-container">
+                <div className="card-top-container user-card-top-container">
                     <div className="card-top-info">  
                         <img src={img} className="icon"></img>
                         <p className="username">{username}</p>
                         <p className="you-tag">you</p>
                         <p className="createdAt"><ReactTimeAgo date={new Date()} locale="en-US"/></p>
                     </div>
-                    <div className="delete-edit-container reply-btn-container">
-                        <img src="../../public/images/icon-delete.svg" className="delete-icon"></img>
+                    <div className="reply-btn-container">
+                        <img src="/images/icon-delete.svg" className="delete-icon icon"></img>
                         <p className="delete-text" onClick={handleToggle}>Delete</p>
-                        <img src="../../public/images/icon-edit.svg" className="edit-icon"></img>
-                        <p className="reply-text">Edit</p>
+                        {/* <img src="/images/icon-edit.svg" className="edit-icon icon"></img>
+                        <p className="reply-text">Edit</p> */}
                     </div>
                 </div>
                 <div className="card-text">
